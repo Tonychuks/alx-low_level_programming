1cdcd7f019fa1c6180b1825feb95372b821a2043
Tonychuks <tonychuks80@gmail.com>
2022-09-09 15:16:44 -0700

    determined

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,11 +5,10 @@
 /**
  *main - Painting 
  * 
- *Return; Always 0 (successful)
+ *Return: Always 0 (success)
  */
-
 int main(void)
  {
- 	puts("\"Programming is like building a multilingual puzzle");
+	 puts("\"Programming is like building a multilingual puzzle");
 	return (0);
  } 	
