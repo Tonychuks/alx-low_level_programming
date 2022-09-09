6aec9fde8bfabc6de07c6299b03bb39051b9aa4a
Tonychuks <tonychuks80@gmail.com>
2022-09-09 15:24:16 -0700

    profound

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,12 @@
-#include <studio.h>
+#include <stdio.h>
 /** 
  * main - Prints the sizeof many var types 
  *
  * Return: Always 0 (Success) 
  */ 
-
 int main(void) 
 
-{ 
-	char a; 
+{ 	char a; 
 	int b; 
 	long int c; 
 	long long int d; 
