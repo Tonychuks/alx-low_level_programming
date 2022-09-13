321f8b8830fa76c24c3c75bc0525450309288972
Tonychuks <tonychuks80@gmail.com>
2022-09-13 14:45:04 -0700

    starter

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+ #include<stdio.h>
+ /**
+ * main - entry piont
+ *
+ * Return: Always 0 (success)
+ */
+ int main(void)
+ {
+	 char *t = "Tonychuks";
+	 
+	 while (*t)
+	 {
+		 _putchar(*t);
+		 t++;
+	 }
+	 _putchar('\n');
+
+	 return (0);
+  } 
+	 
