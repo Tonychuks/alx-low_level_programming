1facda688652cc895781ccc35abee882f357dea9
Tonychuks <tonychuks80@gmail.com>
2022-09-14 04:49:38 -0700

    kindle

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,20 @@
 #include "main.h"
-
 /**
-* print_alphabet_x10 ->prints the lowercase alphabets
+*main - x10
+*print_alphabet_x10 -> prints the lowercase alphabets
 */
 
 void print_alphabet_x10(void);
 {
-	int x;
-	char j;
+	int j;
+	int c;
 
-	for (x = 0; x < 10; x++)
+	for (c = 0; c < 10; c++)
 	{
-	for(j = 'a'; j <= 'z'; j++)
-	{
-		_putchar(j);
+		for (j -'a'; j <= 'z'; j++)
+		{
+		     _putchar(j);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
-	} 
 }
