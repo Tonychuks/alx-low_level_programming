8bb610fce8980381e9dc7737b675dc9e8198002b
Tonychuks <tonychuks80@gmail.com>
2022-09-14 20:41:47 -0700

    calender with c-programing

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#iclude "main.h"
+/**
+* main - prints positive or negative integer
+* Return: always 0 (success)
+*/
+int main(void)
+{
+	int i;
+	i = 0;
+	positive or negative(i);
+	return (0);
+}
