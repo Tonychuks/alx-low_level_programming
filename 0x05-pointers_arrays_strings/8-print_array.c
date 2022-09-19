e263c6faea9aa4751566e2a7763d9018a4bbd20d
Tonychuks <tonychuks80@gmail.com>
2022-09-19 16:48:09 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -5,17 +5,18 @@
 * print_array - prints n digit of an array
 * @a: array
 * @n: number of values
-* Return: a and n inpputs
+* Return: nothing
 */
-void print_array(int a, int n)
+void print_array(int *a, int n)
 {
 	int s;
 
 	for (s = 0; s < n; s++)
 	{
-		printf("%d", a[s]);
 		if (s != n - 1)
-			printf(", ");
+			printf("%d, ", a[s]);
+		else
+			printf("%d", a[s]);
 	}
-	printf("\n");
+	putchar(10);
 }
