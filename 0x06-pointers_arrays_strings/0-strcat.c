dec4513aa44d3eb8b099da021d054669f139eadd
Tonychuks <tonychuks80@gmail.com>
2022-09-21 19:12:26 -0700

    kiogram

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,7 +14,11 @@ char *_strcat(char *dest, char *src)
 	while (dest[index++])
 		dest_len++;
 
-	for (dest = 0, src[index], index++)
+	for (dest = 0, src[index] != 0; index++)
+	{
 		dest[dest_len++] = src[index];
+		dest_len += 1;
+	}
+	dest[dest_len] = '\0';
 	return (dest);
 }
