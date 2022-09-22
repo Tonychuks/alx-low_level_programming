8f6711a3dfc1e93c41892f2e5375ea476fd8efea
Tonychuks <tonychuks80@gmail.com>
2022-09-22 03:39:00 +0100

    Task

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,14 +1,31 @@
-#include "main.h"
-#include <string.h>
-
 /**
-* _strncat -> function to append some charx
-* @dest: first param
-* @src: second param
-* @n: 3rd param
+ * _strncat - a function that concatenates two strings.
+ *
+ * @dest: pointer to destination input
+ * @src: pointer to source input
+ * @n: most number of bytes from @src
+ *
+ * Return: @dest
 */
+
 char *_strncat(char *dest, char *src, int n)
 {
-	strncat(dest, src, n);
+	int c, i;
+
+	c = 0;
+
+	/*find size of dest array*/
+	while (dest[c])
+		c++;
+
+	/**
+	 * src does not need to be null terminated
+	 * if it contains n or more bytes
+	*/
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[c + i] = src[i];
+	/*null terminate dest*/
+	dest[c + i] = '\0';
+
 	return (dest);
 }
